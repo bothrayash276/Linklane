8ab9e9196d962bcd8114484123e2f835991259ce
bothrayash276 <bothrayash276@gmail.com>
2026-05-29 22:26:32 +0530

    login page design started

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -4,16 +4,26 @@ const Login = () => {
   return (
     <>
       <div
-      className='w-full flex flex-col'>
+      className='w-full flex flex-col align-middle justify-center'>
 
         {/* Login Text */}
         <div
         className='
           w-full text-center font-[Jockey_one] text-6xl
-          text-[#00a6ff]
-        '>
+          text-[#006aff]
+          dark:text-[#00a6ff] '>
           Login
         </div>
+
+        {/* Form */}
+        <input 
+        type="email"
+        id='email'
+        placeholder='Email'
+        className='border max-w-sm p-1 px-2
+        dark:border-[#3f3f3f] dark:placeholder:text-[#3f3f3f]
+        border-black
+        ' />
 
       </div>
     </>
